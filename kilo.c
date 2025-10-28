f050eba8b47329391e90bf959e4482ce77802ee2
Jacob M. <jacobmcewen7@gmail.com>
2025-10-28 06:19:41 -0600

    updated text editor

---
+++ kilo.c
@@ -0,0 +1,199 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+/* defines */
+#define KILO_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+
+};
+
+struct editorConfig E;
+
+/*error handling*/
+void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4); //escape sequence, 4 bytes
+	write(STDOUT_FILENO, "\x1b[H", 3); //handle cursor position after clearing the screen
+
+	perror(s); 
+	exit(1); //set return value to a non-zero value to indicate failure
+}
+
+/*terminal*/
+void disableRawMode() {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
+		die("tcsetattr");
+	}
+	
+}
+
+void enableRawMode() {
+		if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+		atexit(disableRawMode);
+
+		struct termios raw = E.orig_termios;
+		//disables certain ctrl input commands
+		raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //disables software flow control (ctrl s and q for stop/starting input) and  newlines created by enter
+		raw.c_oflag &= ~(OPOST);
+		raw.c_cflag &= ~(CS8);
+		raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+		raw.c_cc[VMIN] = 0;
+		raw.c_cc[VTIME] = 1;
+
+
+		if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n]", 4) != 4) return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break; 
+		i++;
+	}
+	buf[i] = '\0';
+	
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+	//printf("\r\n&buf[i]: '%s'\r\n", &buf[i]);
+
+	return 0;
+
+	// editorReadKey();
+
+	// return -1;
+}
+
+int getWindowSize(int *rows, int *cols) {
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		//backup if ioctl fails. positions cursor bottom right and calculates how many rows and cols
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1; 
+		return getCursorPosition(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+/** append buffer **/
+
+struct abuf {
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+	char *new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab-> len += len;
+}
+
+void abFree(struct abuf *ab) {
+	free(ab->b);
+}
+
+/** output **/
+void editorDrawRows(struct abuf *ab) {
+	int y;
+	//placeholder for size of terminal
+	for (y = 0; y < E.screenrows; y++) {
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+				"Kilo editor -- version %s", KILO_VERSION);
+				if (welcomelen > E.screencols) welcomelen = E.screencols;
+				int padding = (E.screencols - welcomelen) / 2;
+				if (padding) {
+					abAppend(ab, "~", 1);
+					padding--;
+				}
+				while (padding--) abAppend(ab, " ", 1);
+				abAppend(ab, welcome, welcomelen);
+		}else {
+			abAppend(ab, "~", 1);
+		}
+
+		abAppend(ab, "\x1b[K", 3);
+		if (y < E.screenrows - 1) {
+			abAppend(ab, "\r\n", 2);
+		}
+	}
+}
+
+void editorRefreshScreen() {
+	struct abuf ab = ABUF_INIT;
+
+	abAppend(&ab, "\x1b[?25l", 6);
+	abAppend(&ab, "\x1b[H", 3);
+	
+	editorDrawRows(&ab);
+	
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
+}
+
+
+/** input **/
+void editorProcessKeypress() {
+	char c = editorReadKey();
+	
+	switch (c) {
+		case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4); //escape sequence, 4 bytes
+		write(STDOUT_FILENO, "\x1b[H", 3); //handle cursor position after clearing the screen
+		exit(0);
+		break;
+	}
+}
+
+/** init **/
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize"); //TODO check
+}
+
+
+int main() {
+	enableRawMode();
+	initEditor();
+
+	while (1) {
+		editorRefreshScreen();
+		editorProcessKeypress();
+	}
+	 return 0;
+}
