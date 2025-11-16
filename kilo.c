940a634cdf4f25f0a166633e76d714cf19842843
Jacob M. <jacobmcewen7@gmail.com>
2025-11-16 14:55:08 -0700

    more work on row offset

--- kilo.c
+++ kilo.c
@@ -38,6 +38,7 @@ typedef struct erow {
 struct editorConfig {
 	int cx, cy; //cursor position on x and y axis respectively
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -183,7 +184,7 @@ void editorAppendRow(char *s, size_t len) {
 	E.numrows++;
 }
 
-
+//run file name in command line as an argument when running the compiled application
 void editorOpen(char *filename) {
 	FILE *fp = fopen(filename, "r");
 	if (!fp) die("fopen");
@@ -231,6 +232,12 @@ void editorScroll() {
 	if (E.cy >= E.rowoff + E.screenrows) {
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols) {
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -255,9 +262,10 @@ void editorDrawRows(struct abuf *ab) {
 				abAppend(ab, "~", 1);
 			}
 		} else {
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if (len < 0) len = 0;
 			if (len > E.screenrows) len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len); //truncate line in case it would go past the end of the screen
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len); //truncate line in case it would go past the end of the screen
 		}
 
 		abAppend(ab, "\x1b[K", 3);
@@ -278,7 +286,7 @@ void editorRefreshScreen() {
 	editorDrawRows(&ab);
 	
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.rowoff) + 1);
 	abAppend(&ab, buf, strlen(buf));	
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -291,6 +299,8 @@ void editorRefreshScreen() {
 /** input **/
 
 void editorMoveCursor(int key) {
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	
 	switch (key) {
 		case ARROW_UP:
 			if(E.cy != 0) {
@@ -308,7 +318,7 @@ void editorMoveCursor(int key) {
 			}
 			break;
 		case ARROW_RIGHT:
-			if(E.cx != E.screencols - 1){
+			if (row && E.cx < row->size) {
 				E.cx++;
 			}
 			break;
@@ -357,6 +367,7 @@ void initEditor() {
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
